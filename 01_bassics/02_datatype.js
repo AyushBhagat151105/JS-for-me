5191924b530ca0efe3aef1966bcb5973d81fab07
AyushBhagat <ayushnbhagat151105@gmail.com>
2024-02-07 14:56:42 +0530

    data types

--- 01_bassics/02_datatype.js
+++ 01_bassics/02_datatype.js
@@ -5,3 +5,22 @@
 
 console.log(3 + 3);
 
+let name = "hitesh"
+let age = 18
+
+// console.table([name,age])
+
+/*
+    data type 
+    number => 2 to power 53
+    bigint
+    string => ""
+    boolean => true/false
+    null => standalon value
+    undefinde => not assine
+    symobl => unique (ID)
+
+    object 
+*/
+
+console.log(typeof null);
