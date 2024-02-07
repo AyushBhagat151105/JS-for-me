30b5c05b1fa7b1e07cdcbb1514c8c24706cab877
AyushBhagat <ayushnbhagat151105@gmail.com>
2024-02-07 16:38:10 +0530

    2 types of memmory

--- 01_bassics/datatypes-summary.js
+++ 01_bassics/datatypes-summary.js
@@ -32,3 +32,25 @@ const myfunction = function(){
     console.log("Hello World");
 }
 
+// +++++++++++++++++++++++++++++++++++++++++++++++++++++
+
+// Stacj(Primitive), Heap(For Non-P) this 2 is type of memmory
+
+// ex
+
+let myYoutubename = "LUCIFET"
+
+let anouthername = myYoutubename
+anouthername = "Chai or Lucifer"
+console.log(anouthername);
+console.log(myYoutubename);
+
+// heap
+let userone = {
+    email: "user@google.com",
+    upi: "user@ybl",
+}
+
+let usertwo = userone
+
+console.log(userone.email);
