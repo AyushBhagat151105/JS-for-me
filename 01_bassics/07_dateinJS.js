8c2ae2b53813f713bc150b5777d465d633615cd4
AyushBhagat <ayushnbhagat151105@gmail.com>
2024-02-07 18:32:31 +0530

    07 added

---
+++ 01_bassics/07_dateinJS.js
@@ -0,0 +1,16 @@
+// Dates
+// console.log(Date());
+
+let mydate = new Date()
+// console.log(mydate.toString());
+// console.log(mydate.toDateString());
+// console.log(mydate.toISOString());
+// console.log(mydate.toLocaleDateString());
+// console.log(mydate.toLocaleString());
+// console.log(mydate.toLocaleTimeString());
+
+// console.log(typeof mydate);
+
+let myCreatedDate = new Date(2005, 10, 15)
+console.log(myCreatedDate.toDateString());
+console.log(myCreatedDate.toLocaleString());
