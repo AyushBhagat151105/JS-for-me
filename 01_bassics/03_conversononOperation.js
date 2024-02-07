012e11511ac10d363d1a073f127bc64b5e64a457
AyushBhagat <ayushnbhagat151105@gmail.com>
2024-02-07 15:31:14 +0530

    operation

--- 01_bassics/03_conversononOperation.js
+++ 01_bassics/03_conversononOperation.js
@@ -28,6 +28,38 @@ let booleanisloggedin = Boolean(isloggedin)
 let someNumber = 33
 
 let stringNumber = String(someNumber)
-console.log(stringNumber);
-console.log(typeof stringNumber);
+// console.log(typeof stringNumber);
+
+// ************* Operations ***************
+
+let value = 3
+let negValue = -value
+// console.log(negValue);
+
+// console.log(2 + 2);
+// console.log(2 - 2);
+// console.log(2 * 2);
+// console.log(2 ** 2);
+// console.log(2 / 2);
+// console.log(2 % 2);
+
+let str1 = "hello"
+let str2 = "Ayush"
+
+let str3 = str1 + str2
+
+// console.log(str3);
+
+console.log(+true);
+console.log(+"");
+
+let num1, num2, num3
+
+num1 = num2 =num3 = 2 + 2
+
+console.log(num1);
+
+let gamecounter = 100
+gamecounter++;
+console.log(gamecounter);
 
