a6ffca0e6bf767ac29ad9d591412496bf7032852
AyushBhagat <ayushnbhagat151105@gmail.com>
2024-02-07 15:15:25 +0530

    converson of data type

---
+++ 01_bassics/03_conversononOperation.js
@@ -0,0 +1,33 @@
+let score = "33"
+
+// console.log(typeof score)
+
+let valueinNumber = Number(score)
+
+// console.log(typeof valueinNumber)
+// console.log(valueinNumber)
+
+/* 
+    string"33" => 33
+      ||  "33 abc" => NaN
+    true => 1;fualse => 0
+*/
+
+let isloggedin = 1
+
+let booleanisloggedin = Boolean(isloggedin)
+
+// console.log(booleanisloggedin);
+
+/* 
+    1 => true; 0 => false
+    "" => false
+    "ayush" => true
+*/
+
+let someNumber = 33
+
+let stringNumber = String(someNumber)
+console.log(stringNumber);
+console.log(typeof stringNumber);
+
