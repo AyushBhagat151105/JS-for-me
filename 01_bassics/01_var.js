31e1154d09063907013dc8a249757ccc989c0713
AyushBhagat <ayushnbhagat151105@gmail.com>
2024-02-07 14:39:00 +0530

    end 01

---
+++ 01_bassics/01_var.js
@@ -0,0 +1,11 @@
+const account = 1444553
+let accountemail = "Ayush@gmail.com"
+var accountpassword = "151105"
+
+// account = 2
+
+accountemail = "ayush@bb.com"
+accountpassword = "15110333"
+
+console.log(account);
+console.table([account,accountemail,accountpassword])
