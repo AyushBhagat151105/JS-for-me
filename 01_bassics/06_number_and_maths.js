76ff6d403db5b004dfaa8166c074c19760324d5d
AyushBhagat <ayushnbhagat151105@gmail.com>
2024-02-07 17:43:10 +0530

    numb & maths

---
+++ 01_bassics/06_number_and_maths.js
@@ -0,0 +1,26 @@
+const score = 400
+
+const balance = new Number(100)
+// console.log(balance.toFixed(2));
+// console.log(score.toString());
+
+const hundreds = 1000000
+// console.log(hundreds.toLocaleString('en-IN'));
+
+// ++++++++++++++++Maths+++++++++++++++++++
+
+// console.log(Math);
+// console.log(Math.abs(-4));
+// console.log(Math.round(4.6));
+// console.log(Math.ceil(4.2));
+// console.log(Math.floor(4.9));
+// console.log(Math.min(4,3,6,8,9));
+
+console.log(Math.floor(Math.random(4)*10) + 1);
+
+const min = 10
+const max = 4000
+
+console.log(Math.floor(Math.random() * (max - min + 1))+ min)
+
+
