c08427b84531f545178701bcb3d376553c822754
AyushBhagat <ayushnbhagat151105@gmail.com>
2024-02-14 19:15:51 +0530

    01 end

---
+++ 02_bassics/01_array.js
@@ -0,0 +1,34 @@
+// array
+
+const myArr = [0, 1, 2, 3, 4, 5]
+
+// console.log(myArr[2])
+
+// console.log(myArr[4])
+
+// myArr.push(6,7)
+// myArr.pop()
+// console.log(myArr)
+// myArr.unshift(9)
+myArr.shift()
+
+// console.log(myArr);
+
+// console.log(myArr.includes(9));
+// console.log(myArr.indexOf(3));
+
+const newArr =myArr.join()
+
+// console.log(newArr);
+
+//slice, splice
+
+console.log("A",myArr);
+const myn1 = myArr.slice(1,3)
+
+console.log(myn1);
+
+
+const myn2 = myArr.splice(1,3)
+console.log(myn2);
+
