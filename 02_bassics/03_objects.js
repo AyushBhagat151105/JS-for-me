8c6a166f747f2af68afce892329aa409ef07bd30
AyushBhagat <ayushnbhagat151105@gmail.com>
2024-02-14 19:45:24 +0530

    03

---
+++ 02_bassics/03_objects.js
@@ -0,0 +1,15 @@
+// singleton
+
+// object literals 
+// Object.create
+
+const Jsuser = {
+    name:"Ayush",
+    age: 18,
+    location:"Gujrat",
+    email: "brbrb.com",
+    isLoggedIn: false,
+    lastloginDays:["Monday","Saturday"]
+}
+
+console.log(Jsuser["email"])
