3c847b53f93a8a831c3307cb5ce1111ea5c24881
AyushBhagat <ayushnbhagat151105@gmail.com>
2024-02-18 18:57:16 +0530

    03

--- 02_bassics/03_objects.js
+++ 02_bassics/03_objects.js
@@ -2,14 +2,36 @@
 
 // object literals 
 // Object.create
+const mysym = Symbol("key1")
 
 const Jsuser = {
     name:"Ayush",
     age: 18,
+    [mysym]: "key1",
     location:"Gujrat",
     email: "brbrb.com",
     isLoggedIn: false,
     lastloginDays:["Monday","Saturday"]
 }
 
-console.log(Jsuser["email"])
+
+// console.log(Jsuser["email"])
+// console.log(Jsuser["location"])
+// console.log(typeof Jsuser[mysym])
+
+// Jsuser.email = "lolol@chatgpt.com"
+// Object.freeze(Jsuser)
+// Jsuser.email = "lol@chatgpt.com"
+
+// console.log(Jsuser);
+
+Jsuser.greeting = function(){
+    console.log("Hii js user");
+}
+
+Jsuser.greetingtwo = function(){
+    console.log(`Hii js user,${this.name}`);
+}
+
+console.log(Jsuser.greeting());
+console.log(Jsuser.greetingtwo());
